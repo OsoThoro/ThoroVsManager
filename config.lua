93d4dd69b7866333be81197600470f9ca8ffcaa3
Oso <87783701+OsoThoro@users.noreply.github.com>
2025-04-07 02:13:25 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -8,5 +8,17 @@ Config.GeneratePricesIfMissing = true
 -- Range for random price generation (low, high)
 Config.PriceRange = { min = 50000, max = 200000 }
 
--- Path to the vehicle pack folder
-Config.VehiclePackPath = "resources/[vehicles]/"
+-- Path to the vehiclse pack folder
+Config.VehiclePacks = {
+    {
+        resourceName = "vehicles_pack_1", -- Replace with your actual resource name
+        resourceLocation = "resources/vehicles_pack_1" -- Adjust the location as needed
+    },
+    {
+        resourceName = "vehicles_pack_2", -- Another resource pack
+        resourceLocation = "resources/vehicles_pack_2"
+    }
+}
+
+-- Whether to automatically start scanning on script start
+Config.EnableScanOnStart = true 
