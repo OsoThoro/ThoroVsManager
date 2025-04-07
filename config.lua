737100ff52f3906125a7bbce2311cf45dcb86a50
Oso <87783701+OsoThoro@users.noreply.github.com>
2025-04-07 01:11:28 -0400

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,16 @@
+Config = {}
+
+-- Price range by vehicle category
+Config.VehiclePriceRange = {
+    compacts = {min = 15000, max = 25000},
+    coupes = {min = 20000, max = 35000},
+    sedans = {min = 22000, max = 40000},
+    sports = {min = 50000, max = 100000},
+    sportsclassics = {min = 40000, max = 80000},
+    super = {min = 100000, max = 200000},
+    muscle = {min = 25000, max = 50000},
+    offroad = {min = 15000, max = 35000},
+    suvs = {min = 30000, max = 60000},
+    vans = {min = 15000, max = 30000},
+    motorcycles = {min = 10000, max = 50000}
+}
