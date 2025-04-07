fe6b05c7d8bac30d76a5cfad1b7ca8aab90569c0
Oso <87783701+OsoThoro@users.noreply.github.com>
2025-04-07 01:11:11 -0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Thorough'
+description 'Vehicle Pack Scanner for ESX'
+version '1.0.0'
+
+server_script {
+    'server.lua',
+    'config.lua'
+}
+
+shared_script 'locales/en.lua'
