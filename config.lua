e9884860016fa46ae7b28a1d64e918a3674b8d70
Oso <87783701+OsoThoro@users.noreply.github.com>
2025-04-07 01:50:59 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,16 +1,12 @@
+-- Configuration file for Vehicle Pack Scanner
+
 Config = {}
 
--- Price range by vehicle category
-Config.VehiclePriceRange = {
-    compacts = {min = 15000, max = 25000},
-    coupes = {min = 20000, max = 35000},
-    sedans = {min = 22000, max = 40000},
-    sports = {min = 50000, max = 100000},
-    sportsclassics = {min = 40000, max = 80000},
-    super = {min = 100000, max = 200000},
-    muscle = {min = 25000, max = 50000},
-    offroad = {min = 15000, max = 35000},
-    suvs = {min = 30000, max = 60000},
-    vans = {min = 15000, max = 30000},
-    motorcycles = {min = 10000, max = 50000}
-}
+-- Whether or not to automatically generate vehicle prices if they are missing
+Config.GeneratePricesIfMissing = true
+
+-- Range for random price generation (low, high)
+Config.PriceRange = { min = 50000, max = 200000 }
+
+-- Path to the vehicle pack folder
+Config.VehiclePackPath = "resources/[vehicles]/"
